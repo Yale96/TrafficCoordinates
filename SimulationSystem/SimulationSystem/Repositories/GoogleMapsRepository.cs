0d2570bceda7a53d21636d5a10eff169cea93e6b
frankie285 <frankievanmeel@hotmail.com>
2018-02-27 15:33:37 +0100

    Completed convertJsonToMakers

--- SimulationSystem/SimulationSystem/Repositories/GoogleMapsRepository.cs
+++ SimulationSystem/SimulationSystem/Repositories/GoogleMapsRepository.cs
@@ -38,11 +38,46 @@ namespace SimulationSystem.Repositories
         public List<Marker> convertJsonToMarkers(string json)
         {
             List<Marker> markers = new List<Marker>();
-            dynamic jsonData = JsonConvert.DeserializeObject<dynamic>(json);
+            int markerID = 0;
+
             string[] firstSplittedString = Regex.Split(json, "\"steps\" : ");
             string[] secondSplittedString = Regex.Split(firstSplittedString[1], ",\n               \"traffic");
-            
-            return null;
+            JArray JSONObjects = JArray.Parse(secondSplittedString[0]);
+            JEnumerable<JToken> tokens = JSONObjects.Children();
+            List<int> indexes = new List<int>();
+            indexes.Add(5);
+
+            for (int i = 0; i < tokens.Count(); i++)
+            {
+                if (i == (tokens.Count() - 1))
+                {
+                    indexes.Add(2);
+                }
+
+                foreach (int index in indexes)
+                {
+                    var JObject = tokens.ElementAt(i);
+                    JEnumerable<JToken> coordinates;
+                    if (index == 5 && JObject.Children().Count() == 8)
+                    {
+                        coordinates = JObject.Children().ElementAt(6).Children();
+                    }
+                    else
+                    {
+                        coordinates = JObject.Children().ElementAt(index).Children();
+                    }
+
+                    double lat;
+                    double lng;
+                    Double.TryParse(coordinates.Children().ElementAt(0).Children().ElementAt(0).ToString(), out lat);
+                    Double.TryParse(coordinates.Children().ElementAt(1).Children().ElementAt(0).ToString(), out lng);
+
+                    Marker m = new Marker(markerID, lat, lng);
+                    markers.Add(m);
+                    markerID++;
+                }
+            }
+            return markers;
         }
 
         public Address getRandomStart()
